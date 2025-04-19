c2002d7c5c22c5d5e344717ad2b9e7b0bc66a0b3
Jethro <qhlrx1@gmail.com>
2025-04-19 14:56:55 +0900

    [Feature] Initial Project

---
+++ Crypto/Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Jethro Liu on 2025/04/19.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
