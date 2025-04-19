b1453d79c8a4e26ccc574aef902dd7619090b54d
Jethro <qhlrx1@gmail.com>
2025-04-19 17:55:25 +0900

    [Feature] Download coins using a View Model and Data Service class

--- Crypto/Crypto/Core/Home/ViewModels/HomeViewModel.swift
+++ Crypto/Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -6,16 +6,24 @@
 //
 
 import Foundation
+import Combine
 
 @Observable class HomeViewModel {
     var allCoins: [CoinModel] = []
     var portfolioCoins: [CoinModel] = []
     
-    init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-        }
-    }
+    private let dataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
     
+    init() {
+        addSubscribers()
+    }
+ 
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+    }
 }
---
+++ Crypto/Crypto/Services/CoinDataService.swift
@@ -0,0 +1,75 @@
+//
+//  CoinDataService.swift
+//  Crypto
+//
+//  Created by Jethro Liu on 2025/04/19.
+//
+
+import Foundation
+import Combine
+
+class CoinDataService {
+    @Published var allCoins: [CoinModel] = []
+    
+    var coinSubscription: AnyCancellable?
+    
+    init() {
+        getCoins()
+    }
+    
+    private func getCoins() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.timeoutInterval = 10
+        request.allHTTPHeaderFields = [
+            "accept": "application/json",
+            "x-cg-demo-api-key": "CG-aJTTkiv5ucQ8hwXFWQQq1GTX"
+        ]
+        
+        let queryItems = [
+            URLQueryItem(name: "vs_currency", value: "usd"),
+            URLQueryItem(name: "order", value: "market_cap_desc"),
+            URLQueryItem(name: "per_page", value: "250"),
+            URLQueryItem(name: "page", value: "1"),
+            URLQueryItem(name: "sparkline", value: "true"),
+            URLQueryItem(name: "price_change_percentage", value: "24h")
+        ]
+        
+        if var components = URLComponents(url: url, resolvingAgainstBaseURL: true) {
+            components.queryItems = queryItems
+            if let urlWithQuery = components.url {
+                request.url = urlWithQuery
+            }
+        }
+        
+        coinSubscription =
+        URLSession.shared.dataTaskPublisher(for: request)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap(handleData)
+            .receive(on: DispatchQueue.main)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print("Failed to fetch data: \(error.localizedDescription)")
+                case .finished:
+                    break
+                }
+            } receiveValue: { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
+            }
+        
+    }
+    
+    private func handleData(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return output.data
+    }
+}
