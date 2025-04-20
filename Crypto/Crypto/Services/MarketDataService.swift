f94f5ff1c511c1f94f37dbea0096250fed62b277
Jethro <qhlrx1@gmail.com>
2025-04-20 17:38:53 +0900

    [Feature] Download and display live market data from API

---
+++ Crypto/Crypto/Services/MarketDataService.swift
@@ -0,0 +1,39 @@
+//
+//  MarketDataService.swift
+//  Crypto
+//
+//  Created by Jethro Liu on 2025/04/20.
+//
+
+import Foundation
+import Combine
+
+class MarketDataService {
+    @Published var marketData: MarketDataModel? = nil
+    
+    var marketDataSubscription: AnyCancellable?
+    
+    init() {
+        getData()
+    }
+    
+    private func getData() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.timeoutInterval = 10
+        request.allHTTPHeaderFields = [
+            "accept": "application/json",
+            "x-cg-demo-api-key": SecretKeyManager.api_key
+        ]
+        
+        marketDataSubscription =
+        NetworkingManager.download(url: request)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedGlobalData in
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscription?.cancel()
+            })
+    }
+}
